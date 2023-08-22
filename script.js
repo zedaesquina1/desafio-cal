503ee3797ff2b3082defb473278344e13a4d47c3
Daniel <116726675+zedaesquina1@users.noreply.github.com>
2023-08-21 23:35:18 -0300

    finalização

---
+++ script.js
@@ -0,0 +1,17 @@
+function addnumero(value) {
+    document.getElementById('display').value += value;
+}
+        
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+        
+function calculate() {
+    try {
+        const expression = document.getElementById('display').value;
+        const result = eval(expression);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Erro';
+}
+}
